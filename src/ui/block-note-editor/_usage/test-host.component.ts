3b92a548aa8397fb6dc35deed77baa326f813b4e
Markus Falk <m.falk@meddevo.com>
2024-07-23 12:58:04 +0200

    refactor: add a few dev tools to the usage component
    refs #4090

--- src/ui/block-note-editor/_usage/test-host.component.ts
+++ src/ui/block-note-editor/_usage/test-host.component.ts
@@ -5,99 +5,57 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { Block } from '@blocknote/core';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { BnaEditorComponent } from '../_component/bna-editor.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, BnaEditorComponent, HlmButtonDirective],
+  imports: [
+    BnaEditorComponent,
+    CommonModule,
+    HlmButtonDirective,
+    ReactiveFormsModule,
+  ],
+  styles: `
+  .btn {
+    margin: 0 1rem;
+    border: 1px solid silver;
+    padding: 0.5rem
+  }
+  fieldset {
+    padding: 1rem;
+    margin: 1rem;
+    border: 1px solid silver;
+  }
+  .invalid-feedback {
+    color: red;
+  }
+  `,
   template: `
     <form [formGroup]="form" (ngSubmit)="submit()">
-      <div class="mb-3">
-        <label class="form-label" for="someControl">Editor</label>
+      <button (click)="disable()" type="button" class="btn">disable</button>
+      <button (click)="enable()" type="button" class="btn">enable</button>
+      <button (click)="patch()" type="button" class="btn">patch</button>
+      <button (click)="reset()" type="button" class="btn">reset</button>
+
+      <fieldset class="mb-3">
+        <label class="form-label" for="someControl">Editor:</label>
         <bna-editor formControlName="editor" />
-        @if (control?.invalid && control?.touched) {
+        @if (control.invalid && control.touched) {
         <em class="invalid-feedback">Required!</em>
         }
-      </div>
+      </fieldset>
+      <button class="btn">submit</button>
       <hr />
-      <pre> {{ form.value | json }} </pre>
+      <pre>{{ form.value | json }}</pre>
     </form>
   `,
 })
 export class TestHostBlockNodeEditorComponent {
   form = this.formBuilder.group({
-    editor: this.formBuilder.control<any | undefined>(
-      [
-        {
-          id: '2b778029-3985-46a1-a30f-756b6b4636db',
-          type: 'paragraph',
-          props: {
-            textColor: 'default',
-            backgroundColor: 'default',
-            textAlignment: 'left',
-          },
-          content: [
-            {
-              type: 'text',
-              text: '1',
-              styles: {},
-            },
-          ],
-          children: [],
-        },
-        {
-          id: 'aece5b48-dd54-435e-9008-26e4b0c35bb4',
-          type: 'paragraph',
-          props: {
-            textColor: 'default',
-            backgroundColor: 'default',
-            textAlignment: 'left',
-          },
-          content: [
-            {
-              type: 'text',
-              text: '2',
-              styles: {},
-            },
-          ],
-          children: [],
-        },
-        {
-          id: '6dcc2772-cadd-4bfa-ab5a-e566e58d4148',
-          type: 'paragraph',
-          props: {
-            textColor: 'default',
-            backgroundColor: 'default',
-            textAlignment: 'left',
-          },
-          content: [
-            {
-              type: 'text',
-              text: '3',
-              styles: {},
-            },
-          ],
-          children: [],
-        },
-        {
-          id: 'a7a34c06-710b-4739-8e82-8681c50d6b4b',
-          type: 'paragraph',
-          props: {
-            textColor: 'default',
-            backgroundColor: 'default',
-            textAlignment: 'left',
-          },
-          content: [
-            {
-              type: 'text',
-              text: '4',
-              styles: {},
-            },
-          ],
-          children: [],
-        },
-      ],
+    editor: this.formBuilder.control<Block[] | undefined>(
+      undefined,
       Validators.required
     ),
   });
@@ -122,37 +80,38 @@ export class TestHostBlockNodeEditorComponent {
   }
 
   patch() {
+    const demoContent: Block[] = [
+      {
+        id: '262bf9c4-3c2a-4543-bc47-49976ec904c3',
+        type: 'paragraph',
+        props: {
+          textColor: 'default',
+          backgroundColor: 'default',
+          textAlignment: 'left',
+        },
+        content: [
+          {
+            type: 'text',
+            text: 'Some Custom text',
+            styles: {},
+          },
+        ],
+        children: [],
+      },
+      {
+        id: '78b618ea-417e-4eea-a432-7baf561a5585',
+        type: 'paragraph',
+        props: {
+          textColor: 'default',
+          backgroundColor: 'default',
+          textAlignment: 'left',
+        },
+        content: [],
+        children: [],
+      },
+    ];
     this.form.patchValue({
-      editor: [
-        {
-          id: '262bf9c4-3c2a-4543-bc47-49976ec904c3',
-          type: 'paragraph',
-          props: {
-            textColor: 'default',
-            backgroundColor: 'default',
-            textAlignment: 'left',
-          },
-          content: [
-            {
-              type: 'text',
-              text: 'Some Custom text',
-              styles: {},
-            },
-          ],
-          children: [],
-        },
-        {
-          id: '78b618ea-417e-4eea-a432-7baf561a5585',
-          type: 'paragraph',
-          props: {
-            textColor: 'default',
-            backgroundColor: 'default',
-            textAlignment: 'left',
-          },
-          content: [],
-          children: [],
-        },
-      ],
+      editor: demoContent,
     });
   }
 
