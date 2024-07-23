c534f79b31e3c2ed3bf66e55e6a1b4de4cc1c498
Markus Falk <m.falk@meddevo.com>
2024-07-23 13:24:49 +0200

    ci: fix lints
    refs #4090

--- src/ui/components/bna-formatting-toolbar/bna-formatting-toolbar.directive.ts
+++ src/ui/components/bna-formatting-toolbar/bna-formatting-toolbar.directive.ts
@@ -1,21 +1,23 @@
-import { Directive, effect, ElementRef, input, OnChanges, Renderer2, SimpleChanges } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  input,
+  OnChanges,
+  Renderer2,
+} from '@angular/core';
 import { BlockNoteEditor } from '@blocknote/core';
 
 @Directive({
   selector: 'bna-formatting-toolbar[editor]',
   standalone: true,
 })
-export class BnaFormattingToolbarDirective implements OnChanges{
+export class BnaFormattingToolbarDirective implements OnChanges {
   editor = input.required<BlockNoteEditor>();
-
 
   constructor(
     protected elRef: ElementRef<HTMLElement>,
     private renderer2: Renderer2
-  ) {
-
-
-  }
+  ) {}
 
   ngOnChanges() {
     const position = this.elRef.nativeElement.getBoundingClientRect();
@@ -23,8 +25,8 @@ export class BnaFormattingToolbarDirective implements OnChanges{
     this.renderer2.setStyle(this.elRef.nativeElement, 'position', 'absolute');
     this.renderer2.setStyle(this.elRef.nativeElement, 'z-index', '10000');
     this.renderer2.addClass(this.elRef.nativeElement, 'Test');
-    if(this.editor()){
-      this.editor().formattingToolbar.onUpdate( (formattingToolbar) => {
+    if (this.editor()) {
+      this.editor().formattingToolbar.onUpdate((formattingToolbar) => {
         console.log(formattingToolbar);
         if (formattingToolbar.show) {
           this.renderer2.setStyle(this.elRef.nativeElement, 'display', 'block');
@@ -46,7 +48,6 @@ export class BnaFormattingToolbarDirective implements OnChanges{
           this.renderer2.setStyle(this.elRef.nativeElement, 'display', 'none');
         }
       });
-
     }
   }
 }
