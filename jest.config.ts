7f57e7b49b10ef09bdb996f6110deda22596a042
Markus Falk <m.falk@meddevo.com>
2024-07-23 14:21:42 +0200

    test: fix setup for require()
    refs #4090

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,21 @@
 import { getJestProjectsAsync } from '@nx/jest';
 
-export default async () => ({
-projects: await getJestProjectsAsync()
-});
+export default {
+  ...async () => ({
+    projects: await getJestProjectsAsync(),
+  }),
+  transform: {
+    '^.+\\.(cjs|ts|tsx|js|jsx|mjs)$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        isolatedModules: true,
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.cjs$|@blocknote)'],
+  moduleNameMapper: {
+    // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
+    uuid: require.resolve('uuid'),
+  },
+};
