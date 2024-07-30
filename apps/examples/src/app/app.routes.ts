948d362da6d7552b9f9ff2e37421662db604f5d2
Matthias Risto <risto.matthias@gmail.com>
2024-07-30 13:31:35 +0200

    feat: change title, add basic styling

--- apps/examples/src/app/app.routes.ts
+++ apps/examples/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { BasicMinimal } from '../basic/minimal/basic-minimal.component';
 import { CustomAlertBlockPage } from '../custom/alert-block/custom-alert-block.page';
 
 export const appRoutes: Route[] = [
+  { path: '', redirectTo: 'basic/minimal', pathMatch: 'full' },
   { path: 'basic/minimal', component: BasicMinimal },
   {
     path: 'custom/alert-block',
