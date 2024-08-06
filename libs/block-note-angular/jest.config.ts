df4ff68bde5205ab70e60a15165fa286dff28839
Matthias Risto <risto.matthias@gmail.com>
2024-08-06 08:24:39 +0200

    refactor: change old library names with the right name block-note-angular

--- libs/block-note-angular/jest.config.ts
+++ libs/block-note-angular/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'core',
+  displayName: 'block-note-angular',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../coverage/libs/block-note-angular/',
