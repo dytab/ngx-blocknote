c907e38ce6d3aa3d80339d7745cb69f230f22533
Markus Falk <m.falk@meddevo.com>
2024-08-08 14:21:55 +0200

    docs: read content from editor
    refs #4131

--- docs/src/pages/examples/basic/basic-setup/basic-setup.example.ts
+++ docs/src/pages/examples/basic/basic-setup/basic-setup.example.ts
@@ -1,15 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Block } from '@blocknote/core';
 import { BnaEditorComponent } from '@dytab/block-note-angular';
 
 @Component({
   selector: 'bna-basic-setup-example',
   standalone: true,
   imports: [CommonModule, BnaEditorComponent],
-  template: ` <bna-editor [initialContent]="initialContent" /> `,
+  templateUrl: 'basic-setup.example.html',
 })
 export class BasicSetupExample {
-  initialContent = undefined;
+  editorContent!: Block[];
+
+  readTextFromEditor(blocks: Block[]) {
+    this.editorContent = blocks;
+  }
 }
 
 export const basicSetupExampleCode = `import { CommonModule } from '@angular/common';
@@ -19,8 +24,12 @@ import { BnaEditorComponent } from '@dytab/block-note-angular';
 @Component({
   standalone: true,
   imports: [CommonModule, BnaEditorComponent],
-  template: \` <bna-editor [initialContent]="initialContent" /> \`,
+  template: \` <bna-editor (contentChanged)="readTextFromEditor($event)" /> \`,
 })
 export class BasicSetupExample {
-  initialContent = undefined;
+  editorContent!: Block[];
+
+  readTextFromEditor(blocks: Block[]) {
+    this.editorContent = blocks;
+  }
 }`;
