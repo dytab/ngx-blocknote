c412b6a57908a370becdbd666c7aa167ce100801
Matthias Risto <risto.matthias@gmail.com>
2024-08-13 17:43:45 +0200

    refactor: remove debug log

--- libs/block-note-angular/src/lib/components/suggestions-menu/bna-suggestions-menu-controller.directive.ts
+++ libs/block-note-angular/src/lib/components/suggestions-menu/bna-suggestions-menu-controller.directive.ts
@@ -64,7 +64,6 @@ export class BnaSuggestionsMenuControllerDirective {
                 ],
               }
             );
-            console.log(result);
             this.renderer2.setStyle(
               this.elRef.nativeElement,
               'top',
