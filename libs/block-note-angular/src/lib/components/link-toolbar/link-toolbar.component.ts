e2e912a25eb2d7481dedcd73515421e329ab1046
Matthias Risto <m.risto@meddevo.com>
2024-08-14 07:40:54 +0200

    refactor(docs): add link toolbar structure files

---
+++ libs/block-note-angular/src/lib/components/link-toolbar/link-toolbar.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bna-link-toolbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './link-toolbar.component.html',
+  styleUrl: './link-toolbar.component.css',
+})
+export class BnaLinkToolbarComponent {}
