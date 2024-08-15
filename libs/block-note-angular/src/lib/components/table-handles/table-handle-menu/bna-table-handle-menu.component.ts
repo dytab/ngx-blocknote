ad8f2e5303f7dce7fea15c794e2175774a0d732b
Matthias Risto <m.risto@meddevo.com>
2024-08-15 08:56:33 +0200

    refactor: add correct positioned table handle menu

--- libs/block-note-angular/src/lib/components/table-handles/table-handle-menu/bna-table-handle-menu.component.ts
+++ libs/block-note-angular/src/lib/components/table-handles/table-handle-menu/bna-table-handle-menu.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'bna-table-handle-menu',
@@ -7,5 +7,9 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './bna-table-handle-menu.component.html',
   styleUrl: './bna-table-handle-menu.component.css',
+  host: {
+    //TODO: change to class not tailwind classes
+    class: 'block bg-background shadow-xl p-2 rounded',
+  },
 })
 export class BnaTableHandleMenuComponent {}
--- libs/block-note-angular/src/lib/components/table-handles/table-handle/bna-table-handle.component.ts
+++ libs/block-note-angular/src/lib/components/table-handles/table-handle/bna-table-handle.component.ts
@@ -1,21 +1,22 @@
 import { CommonModule } from '@angular/common';
 import {
-  AfterViewInit,
   Component,
   computed,
+  effect,
   ElementRef,
+  HostListener,
   input,
   signal,
   viewChild,
 } from '@angular/core';
-import {
-  HlmButtonDirective,
-  HlmIconComponent,
-} from '@dytab/block-note-angular';
+
 import { computePosition, flip } from '@floating-ui/dom';
 import { provideIcons } from '@ng-icons/core';
 import { lucideGripHorizontal, lucideGripVertical } from '@ng-icons/lucide';
 import { BrnMenuTriggerDirective } from '@spartan-ng/ui-menu-brain';
+import { BlockNoteAngularService } from '../../../services';
+import { HlmButtonDirective, HlmIconComponent } from '../../../ui';
+import { BnaTableHandleMenuComponent } from '../table-handle-menu/bna-table-handle-menu.component';
 
 const icons = {
   row: 'lucideGripVertical',
@@ -30,12 +31,30 @@ const icons = {
     HlmButtonDirective,
     HlmIconComponent,
     BrnMenuTriggerDirective,
+    BnaTableHandleMenuComponent,
   ],
   templateUrl: './bna-table-handle.component.html',
   styleUrl: './bna-table-handle.component.css',
   providers: [provideIcons({ lucideGripVertical, lucideGripHorizontal })],
 })
-export class BnaTableHandleComponent implements AfterViewInit {
+export class BnaTableHandleComponent {
+  @HostListener('document:click', ['$event'])
+  onOutsideClick(event: MouseEvent) {
+    const menu = this.menu()?.nativeElement;
+    if (!menu || !event.target) {
+      return;
+    }
+    console.log('im here');
+    const target = event.target as HTMLElement;
+    if (this.isMenuOpened) {
+      console.log('click', this.isMenuOpened);
+      const clickedInside = menu.contains(target);
+      if (!clickedInside) {
+        this.isMenuOpened = false;
+        this.blockNoteAngularService.editor().tableHandles!.unfreezeHandles();
+      }
+    }
+  }
   orientation = input.required<'row' | 'column'>();
   index = input.required<number>();
   icon = computed(() => {
@@ -46,27 +65,40 @@ export class BnaTableHandleComponent implements AfterViewInit {
   button = viewChild<ElementRef<HTMLElement>>('button');
   menu = viewChild<ElementRef<HTMLElement>>('menu');
 
-  isMenuOpened = false;
-
-  async ngAfterViewInit() {
-    const result = await computePosition(
-      this.button()!.nativeElement,
-      this.menu()!.nativeElement,
-      {
-        placement: 'right',
-        strategy: 'absolute',
-        middleware: [flip()],
+  constructor(private blockNoteAngularService: BlockNoteAngularService) {
+    effect(async () => {
+      const button = this.button()?.nativeElement;
+      const menu = this.menu()?.nativeElement;
+      if (!menu || !button) {
+        return;
       }
-    );
-    console.log(result);
-    this.styles.set(this.getStyles(result.y, result.x));
+      const result = await computePosition(
+        this.button()!.nativeElement,
+        this.menu()!.nativeElement,
+        {
+          placement: this.orientation() === 'row' ? 'top' : 'left',
+          strategy: 'absolute',
+          middleware: [flip()],
+        }
+      );
+      console.log(result);
+      this.styles.set(this.getStyles(result.y, result.x));
+    });
   }
+
+  isMenuOpened = false;
 
   getStyles(x: number, y: number) {
     return { top: y + 'px', left: x + 'px' } as const;
   }
 
-  openMenu() {
+  openMenu(event: MouseEvent) {
+    event.stopPropagation();
+    const tableHandles = this.blockNoteAngularService.editor().tableHandles;
+    if (!tableHandles) {
+      return;
+    }
+    tableHandles.freezeHandles();
     this.isMenuOpened = true;
   }
 }
