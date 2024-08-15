e3e4c4996c875fb5c132ad44cbe7dfbe0089ac2e
Matthias Risto <risto.matthias@gmail.com>
2024-08-15 10:19:55 +0200

    test: update some spec files

--- libs/block-note-angular/src/lib/editor/bna-editor.component._spec.ts
+++ libs/block-note-angular/src/lib/editor/bna-editor.component._spec.ts
@@ -2,15 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BnaEditorComponent } from './bna-editor.component';
 
 describe('BnaEditorComponent', () => {
-  let component: BnaEditorComponent;
-  let fixture: ComponentFixture<BnaEditorComponent>;
+  let component: BnaEditorComponent<any, any, any>;
+  let fixture: ComponentFixture<BnaEditorComponent<any, any, any>>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [BnaEditorComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(BnaEditorComponent);
+    fixture = TestBed.createComponent(BnaEditorComponent<any, any, any>);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
