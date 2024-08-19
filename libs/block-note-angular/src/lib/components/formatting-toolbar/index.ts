09905285f1b41183345931c6bed59bc51847f972
Matthias risto <risto.matthias@gmail.com>
2024-08-19 15:03:58 +0200

    fix: add missing exports from formatting toolbar

--- libs/block-note-angular/src/lib/components/formatting-toolbar/index.ts
+++ libs/block-note-angular/src/lib/components/formatting-toolbar/index.ts
@@ -3,3 +3,6 @@ export * from './bna-formatting-toolbar.component';
 export * from './default-buttons/basic-text-style-button/bna-basic-text-style-button.component';
 export * from './default-buttons/text-align-button/bna-text-align-button.component';
 export * from './default-selects/block-type-select/default-block-type-select-items';
+export * from './default-selects/block-type-select/bna-block-type-select.component';
+export * from './default-buttons/create-link/bna-create-link.component';
+export * from './default-buttons/color-style/bna-color-style-button.component';
