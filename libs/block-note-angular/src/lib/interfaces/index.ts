8e75a981b3162e6d27cd0e21ae0529959d02d2c0
Matthias risto <risto.matthias@gmail.com>
2024-08-19 14:53:03 +0200

    fix: export missing types slash-menu-items

--- libs/block-note-angular/src/lib/interfaces/index.ts
+++ libs/block-note-angular/src/lib/interfaces/index.ts
@@ -1,3 +1,5 @@
 export * from './block-note-editor-options.type';
 export * from './color-options.type';
 export * from './block-type-select-item';
+export * from './slash-menu-items.type';
+export * from './table-handle-options.type';
