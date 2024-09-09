a7dc3f9dff1a1a15a6096c806feea49d091d0407
Johannes Klein <j.klein@meddevo.com>
2024-09-09 19:38:01 +0200

    build: get all paths

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -1,8 +1,16 @@
 /// <reference types="vitest" />
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import fs from 'fs';
 import analog from '@analogjs/platform';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
+const links = fs
+  .readFileSync('apps/docs/src/app/pages/examples/shared/examples.ts', 'utf-8')
+  .split('\n')
+  .filter((line: string) => line.trim().match(/url:\s*'([^']+)'/))
+  .map((line: string) => line.trim().split("url: '")[1].split("'")[0]);
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -22,8 +30,13 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [
       analog({
+        static: true,
         prerender: {
-          routes: ['/', '/overview', '/examples'],
+          routes: [
+            '/',
+            '/overview',
+            ...links.map((link: string) => `/examples/${link}`),
+          ],
         },
       }),
       nxViteTsPaths(),
