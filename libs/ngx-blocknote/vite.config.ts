69493b9f215f3ba0190ce27b271751e80ccdb9f4
Matthias Risto <m.risto@meddevo.com>
2025-04-18 11:09:52 +0200

    feat: build ngx-blocknote with vite

--- libs/ngx-blocknote/vite.config.ts
+++ libs/ngx-blocknote/vite.config.ts
@@ -1,23 +1,58 @@
-/// <reference types="vitest" />
-
-import { defineConfig } from 'vite';
+/// <reference types='vitest' />
 import angular from '@analogjs/vite-plugin-angular';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    root: __dirname,
-    plugins: [angular()],
-    test: {
-      reporters: ['default'],
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: ['src/test-setup.ts'],
-      include: ['**/*.spec.ts'],
-      cacheDir: `../../node_modules/.vite`,
+export default defineConfig(() => ({
+  root: __dirname,
+  ssr: {
+    noExternal: ['blocknote'],
+  },
+  plugins: [
+    angular(),
+    nxViteTsPaths(),
+    // nxCopyAssetsPlugin(['*.md']),
+    // dts({
+    //   entryRoot: 'src',
+    //   tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
+    // }),
+  ],
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+  // Configuration for building your library.
+  // See: https://vitejs.dev/guide/build.html#library-mode
+  build: {
+    outDir: '../../dist/ngx-blocknote',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
     },
-    define: {
-      'import.meta.vitest': mode !== 'production',
+    lib: {
+      // Could also be a dictionary or array of multiple entry points.
+      entry: 'src/index.ts',
+      name: 'ngx-blocknote',
+      fileName: 'index',
+      // Change this to the formats you want to support.
+      // Don't forget to update your package.json as well.
+      formats: ['es' as const],
     },
-  };
-});
+    rollupOptions: {
+      // External packages that should not be bundled into your library.
+      external: [],
+    },
+  },
+  test: {
+    watch: false,
+    globals: true,
+    environment: 'node',
+    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../coverage/ngx-blocknote',
+      provider: 'v8' as const,
+    },
+  },
+}));
