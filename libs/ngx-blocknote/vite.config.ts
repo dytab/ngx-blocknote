04565c461c56048d1ff1d4a9b27f8cc56e4ae3c1
Johannes Klein <j.klein@meddevo.com>
2025-05-11 12:06:22 +0200

    refactor: remove unused dts plugin from Vite config

--- libs/ngx-blocknote/vite.config.ts
+++ libs/ngx-blocknote/vite.config.ts
@@ -2,22 +2,12 @@
 import angular from '@analogjs/vite-plugin-angular';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import * as path from 'path';
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
 
 export default defineConfig(() => ({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/libs/ngx-blocknote',
-  plugins: [
-    angular(),
-    nxViteTsPaths(),
-    nxCopyAssetsPlugin(['*.md']),
-    dts({
-      entryRoot: 'src',
-      tsconfigPath: path.join(__dirname, 'tsconfig.app.json'),
-    }),
-  ],
+  plugins: [angular(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
   resolve: {
     mainFields: ['module'],
   },
